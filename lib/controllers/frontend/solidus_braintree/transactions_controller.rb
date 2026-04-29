afc7b39d13a22ef3aa835bac52a3ffbf4881393d
Simon Meyborg <meyborg@syborgstudios.com>
2026-04-29 14:01:31 +0200

    Replace Spree::StoreController with Spree::BaseController
    Spree::StoreController was provided by solidus_frontend which has
    been removed. Spree::BaseController from solidus_core provides the
    same base functionality.

--- lib/controllers/frontend/solidus_braintree/transactions_controller.rb
+++ lib/controllers/frontend/solidus_braintree/transactions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SolidusBraintree
-  class TransactionsController < ::Spree::StoreController
+  class TransactionsController < ::Spree::BaseController
     class InvalidImportError < StandardError; end
 
     PERMITTED_BRAINTREE_TRANSACTION_PARAMS = [
